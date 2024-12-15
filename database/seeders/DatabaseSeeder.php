5bb2739aa21bb4b16082d7addf15a39ce6e56f2e
Tomasz Nowacki <tomasz.nowacki@wsb.poznan.pl>
2024-12-15 10:15:20 +0100

    Faktoryzacja oraz seedowanie dla postów

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
-        // ]);
+        // ]); 
+        
         $this->call(PostSeeder::class);
     }
 }
